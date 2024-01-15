d7aca8f86c68a6536732a9f06e0638c17eb17e9e
Andrew Shkolik <andrew.shkolik@selerix.com>
2024-01-15 01:08:56 -0600

    - path by edge and point now supported, but not recommended to use, since hot wire will burn foam near the point since it's not moving whole time there. Better use helper geometry to make at least small edge that came from point.

--- Path.py
+++ Path.py
@@ -12,9 +12,20 @@ import FreeCADGui
 Gui=FreeCADGui
 import Part
 import utilities
+import math
+
+'''
+    Calculates distance between 2 vertexes in 3d space
+    @param v1 - Vertex 1
+    @param v2 - Vertex 2
+'''
+def distanceToVertex(v1, v2):
+    return math.sqrt((v2.X - v1.X)**2 + (v2.Y - v1.Y)**2 + (v2.Z - v1.Z)**2) 
 
 '''
   Synchronize direction of two edges using their end vertexes
+  Sometimes it can produce wrong result (for example for heavy sweeped wing where tip leading edge past root trailing edge)
+  Take it into account and check if reversing one set of vertexes produce shortest line when projecting to working planes
   @return (vertex00, vertex01, vertex10, vertex11)
 '''
 def getSynchronizedVertices(first, second):
@@ -27,6 +38,7 @@ def getSynchronizedVertices(first, second):
         second.firstVertex()  if second.firstVertex().Point == v2 else second.lastVertex(),
         second.lastVertex()   if second.firstVertex().Point == v2 else second.firstVertex(),
     )
+
 
 '''
   Make path on working planes by two sets of points
@@ -46,18 +58,50 @@ def makePathByPointSets(first, second, planes):
 
     # - Initialize result
     result = []
+    
+    pathsLength = []
+    
+    # try inverted edge only if we are working with edges, not with vertexes
+    examineLength = len(first) > 1 and  len(second) > 1
 
     # - Intersect line by each point pair and each plane
     for plane_index in range(len(planes)):
         plane_points = []
-        for point_index in range(len(first)):
+        for point_index in range(len(first)):            
             plane_points.append(
                 utilities.intersectLineAndPlane(first[point_index], second[point_index], planes[plane_index])
             )
+        if examineLength:
+            pathsLength.append(distanceToVertex(plane_points[0], plane_points[len(plane_points) - 1]))        
         result.append(plane_points)
 
-    # - Done
+    # check with one edge reversed
+    # if resulted length will be less then use this points order
+    if(examineLength):
+        resultInverted = []
+        pathsLengthInverted = []
+        # - Intersect line by each point pair and each plane
+        for plane_index in range(len(planes)):
+            plane_points = []
+            for point_index in range(len(first)):            
+                plane_points.append(                
+                    utilities.intersectLineAndPlane(first[len(first) - point_index - 1], second[point_index], planes[plane_index])
+                )
+            pathsLengthInverted.append(distanceToVertex(plane_points[0], plane_points[len(plane_points) - 1]))    
+            resultInverted.append(plane_points)
+
+        invert = True
+
+        # if any edge from normal list shorter than same edge from inverted list - use normal list
+        for pathIndex in range(len(pathsLength)):
+            if pathsLength[pathIndex] < pathsLengthInverted[pathIndex]:
+                invert = False
+                break
+        
+        # - Done
+        return resultInverted if invert else result
     return result
+
 
 '''
   Make path on working planes by two edges, vertices, or their combination
@@ -65,27 +109,15 @@ def makePathByPointSets(first, second, planes):
   @param second - Second edge / vertex
   @param step - Distance between points in edge discretization
 '''
-def makePathPointsByEdgesPair(first, second, planes, invertEdge = False, step = 0.1):
+def makePathPointsByEdgesPair(first, second, planes, step = 0.1):
     # --- Use only end vertices of coplanar edges or lines because path will be a straight line
     if first.isCoplanar(second) or (first.ShapeType == "Edge" and first.Curve.TypeId == "Part::GeomLine" and  second.ShapeType == "Edge" and second.Curve.TypeId == "Part::GeomLine"):
         
         # - Synchronize edges direction
-        # TODO: Fix inacurate determination of points pair - sometimes it got flipped
-        # for now there is dirty fix with InvertEdge property on path, but at least it makes 
-        # Path tool less annoying
+        # sometime it produces incorrect result, so one points set needs to be flipped. 
+        # it's known issue, and method makePathByPointSets() accounts for it
         v00, v01, v10, v11 = getSynchronizedVertices(first, second)
         
-        # - try to fix calculation error
-        if invertEdge:
-            if v00.Point != v01.Point:
-                vtemp = v00
-                v00 = v01
-                v01 = vtemp
-            else:
-                vtemp = v10
-                v10 = v11
-                v11 = vtemp
-
         # - Make path
         return makePathByPointSets([v00.Point, v01.Point], [v10.Point, v11.Point], planes)
 
@@ -138,8 +170,7 @@ class PathSection:
 
         obj.addProperty("App::PropertyLinkSub",     "LeftEdge",             "Edges",    "Left Edge").LeftEdge = edge_l
         obj.addProperty("App::PropertyLinkSub",     "RightEdge",            "Edges",    "Right Edge").RightEdge = edge_r
-        obj.addProperty("App::PropertyBool",        "InvertEdge",           "Edges",    "Check in case of obvious error in calculations").InvertEdge = False
-
+        
         obj.addProperty("App::PropertyInteger",     "PointsCount",          "Information", "Number of points", 1)
         obj.addProperty("App::PropertyDistance",    "LeftSegmentLength",    "Information", "Left Segment length",   1)
         obj.addProperty("App::PropertyDistance",    "RightSegmentLength",   "Information", "Right Segment length",   1)
@@ -161,11 +192,11 @@ class PathSection:
         if wp is None or len(wp) != 2:
             FreeCAD.Console.PrintError("ERROR:\n Error updating Path - working planes not found in Parent object '{}'\n".format(job.Label if job is not None else "None"))
 
-        edge_l = obj.LeftEdge[0].getSubObject(obj.LeftEdge[1])[0]
-        edge_r = obj.RightEdge[0].getSubObject(obj.RightEdge[1])[0]
+        left = obj.LeftEdge[0].getSubObject(obj.LeftEdge[1])[0]
+        right = obj.RightEdge[0].getSubObject(obj.RightEdge[1])[0]
         
         # - Make path between objects on working planes
-        path_points = makePathPointsByEdgesPair(edge_l, edge_r, wp, obj.InvertEdge)
+        path_points = makePathPointsByEdgesPair(left, right, wp)
 
         # - Set data
         obj.Path_L       = [App.Vector(item.X, item.Y, item.Z) for item in path_points[0]]
@@ -182,7 +213,7 @@ class PathSection:
         path_R.approximate(Points = obj.Path_R, Continuity="C0")
 
         # - Update shape and information
-        obj.Shape = Part.makeCompound([path_L.toShape(), path_R.toShape(), Part.Wire(edge_l), Part.Wire(edge_r)])
+        obj.Shape = Part.makeCompound([path_L.toShape(), path_R.toShape(), left, right])
         obj.LeftSegmentLength = float(path_L.length())
         obj.RightSegmentLength = float(path_R.length())
 
@@ -233,11 +264,11 @@ class MakePath():
     def Activated(self):
         group = Gui.ActiveDocument.ActiveView.getActiveObject("group")
         if group is not None and group.Type == "Job":
-            # - Get selected edges
-            edges = utilities.getAllSelectedEdges()
+            # - Get selected objects
+            objects = utilities.getAllSelectedObjects()
             obj = group.newObject("Part::FeaturePython","Path")
             
-            PathSection(obj, (FreeCAD.ActiveDocument.getObject((edges[0])[0].Name), (edges[0])[1][0]), (FreeCAD.ActiveDocument.getObject((edges[1])[0].Name),(edges[1])[1][0]))
+            PathSection(obj, (FreeCAD.ActiveDocument.getObject((objects[0])[0].Name), (objects[0])[1][0]), (FreeCAD.ActiveDocument.getObject((objects[1])[0].Name),(objects[1])[1][0]))
             PathSectionVP(obj.ViewObject)
             obj.ViewObject.PointSize = 4
 
@@ -250,11 +281,11 @@ class MakePath():
         else:
             group = Gui.ActiveDocument.ActiveView.getActiveObject("group")
             if group is not None and group.Type == "Job":  
-                # - Get selected edges
-                edges = utilities.getAllSelectedEdges()
+                # - Get selected objects
+                objects = utilities.getAllSelectedObjects()
 
                 # - Number of edges should be two
-                if len(edges) != 2:               
+                if len(objects) != 2:               
                     return False
                 
                 wp = utilities.getWorkingPlanes(group)
