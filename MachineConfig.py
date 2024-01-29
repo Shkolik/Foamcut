02df87e7e5fd5e5eab530f5f65d24c38e2e50422
Andrew Shkolik <andrew.shkolik@selerix.com>
2024-01-29 02:08:21 -0600

    - default machine configuration now stored in workbench parameters

--- MachineConfig.py
+++ MachineConfig.py
@@ -15,32 +15,32 @@ class MachineConfig:
     def __init__(self, obj):
         obj.addProperty("App::PropertyString",      "Type",       "", "", 5).Type = "Helper"
         
-        obj.addProperty("App::PropertyLength",     "HorizontalTravel", "Machine Geometry",  "Horizontal travel distance").HorizontalTravel = 800
-        obj.addProperty("App::PropertyLength",     "VerticalTravel",   "Machine Geometry",  "Vertical travel distance"  ).VerticalTravel = 300   # - Vertical travel
-        obj.addProperty("App::PropertyLength",     "FieldWidth",       "Machine Geometry",  "Distance between wire ends").FieldWidth = 500   # - Width
+        obj.addProperty("App::PropertyLength",     "HorizontalTravel", "Machine Geometry",  "Horizontal travel distance").HorizontalTravel = getParameterFloat("HorizontalTravel", 550)
+        obj.addProperty("App::PropertyLength",     "VerticalTravel",   "Machine Geometry",  "Vertical travel distance"  ).VerticalTravel = getParameterFloat("VerticalTravel", 300)
+        obj.addProperty("App::PropertyLength",     "FieldWidth",       "Machine Geometry",  "Distance between wire ends").FieldWidth = getParameterFloat("FieldWidth", 730)
         
-        obj.addProperty("App::PropertyString",     "X1AxisName",        "Axis Mapping",     "Name of X1 axis in GCODE").X1AxisName = "X"
-        obj.addProperty("App::PropertyString",     "Z1AxisName",        "Axis Mapping",     "Name of Z1 axis in GCODE").Z1AxisName = "Y"
-        obj.addProperty("App::PropertyString",     "X2AxisName",        "Axis Mapping",     "Name of X2 axis in GCODE").X2AxisName = "Z"
-        obj.addProperty("App::PropertyString",     "Z2AxisName",        "Axis Mapping",     "Name of Z2 axis in GCODE").Z2AxisName = "A"
-        obj.addProperty("App::PropertyString",     "R1AxisName",        "Axis Mapping",     "Name of rotary table axis in GCODE").R1AxisName = "B"
+        obj.addProperty("App::PropertyString",     "X1AxisName",        "Axis Mapping",     "Name of X1 axis in GCODE").X1AxisName = getParameterString("X1AxisName", "X")
+        obj.addProperty("App::PropertyString",     "Z1AxisName",        "Axis Mapping",     "Name of Z1 axis in GCODE").Z1AxisName = getParameterString("Z1AxisName","Y")
+        obj.addProperty("App::PropertyString",     "X2AxisName",        "Axis Mapping",     "Name of X2 axis in GCODE").X2AxisName = getParameterString("X2AxisName", "Z")
+        obj.addProperty("App::PropertyString",     "Z2AxisName",        "Axis Mapping",     "Name of Z2 axis in GCODE").Z2AxisName = getParameterString("Z2AxisName", "A")
+        obj.addProperty("App::PropertyString",     "R1AxisName",        "Axis Mapping",     "Name of rotary table axis in GCODE").R1AxisName = getParameterString("R1AxisName", "B")
 
-        obj.addProperty("App::PropertyDistance",    "HomingX1",         "Homing",           "Initial position for X1 axis").HomingX1 = 10
-        obj.addProperty("App::PropertyDistance",    "HomingZ1",         "Homing",           "Initial position for Z1 axis").HomingZ1 = 290
-        obj.addProperty("App::PropertyDistance",    "HomingX2",         "Homing",           "Initial position for X2 axis").HomingX2 = 10
-        obj.addProperty("App::PropertyDistance",    "HomingZ2",         "Homing",           "Initial position for Z2 axis").HomingZ2 = 290
-        obj.addProperty("App::PropertyDistance",    "HomingR1",         "Homing",           "Initial position for R1 axis").HomingR1 = 0
+        obj.addProperty("App::PropertyDistance",    "HomingX1",         "Homing",           "Initial position for X1 axis").HomingX1 = getParameterFloat("HomingX1", 10)
+        obj.addProperty("App::PropertyDistance",    "HomingZ1",         "Homing",           "Initial position for Z1 axis").HomingZ1 = getParameterFloat("HomingZ1", 290)
+        obj.addProperty("App::PropertyDistance",    "HomingX2",         "Homing",           "Initial position for X2 axis").HomingX2 = getParameterFloat("HomingX2", 10)
+        obj.addProperty("App::PropertyDistance",    "HomingZ2",         "Homing",           "Initial position for Z2 axis").HomingZ2 = getParameterFloat("HomingZ2", 290)
+        obj.addProperty("App::PropertyDistance",    "HomingR1",         "Homing",           "Initial position for R1 axis").HomingR1 = getParameterFloat("HomingR1", 0)
 
-        obj.addProperty("App::PropertyDistance",    "ParkX",            "Parking",          "Parking position for X").ParkX = 10
-        obj.addProperty("App::PropertyDistance",    "ParkZ",            "Parking",          "Parking position for Z").ParkZ = 290
-        obj.addProperty("App::PropertyDistance",    "ParkR1",           "Parking",          "Parking position for rotary table").ParkR1 = 0
+        obj.addProperty("App::PropertyDistance",    "ParkX",            "Parking",          "Parking position for X").ParkX = getParameterFloat("ParkX", 10)
+        obj.addProperty("App::PropertyDistance",    "ParkZ",            "Parking",          "Parking position for Z").ParkZ = getParameterFloat("ParkZ", 290)
+        obj.addProperty("App::PropertyDistance",    "ParkR1",           "Parking",          "Parking position for rotary table").ParkR1 = getParameterFloat("ParkR1", 0)
 
-        obj.addProperty("App::PropertySpeed",      "FeedRateCut",       "FeedRate",         "Feed rate while cutting").FeedRateCut = 7
-        obj.addProperty("App::PropertySpeed",      "FeedRateMove",      "FeedRate",         "Feed rate while moving").FeedRateMove = 14
-        obj.addProperty("App::PropertySpeed",      "FeedRateRotate",    "FeedRate",         "Feed rate while rotating").FeedRateRotate = 30
+        obj.addProperty("App::PropertySpeed",      "FeedRateCut",       "FeedRate",         "Feed rate while cutting").FeedRateCut = getParameterFloat("FeedRateCut", 7)
+        obj.addProperty("App::PropertySpeed",      "FeedRateMove",      "FeedRate",         "Feed rate while moving").FeedRateMove = getParameterFloat("FeedRateMove", 30)
+        obj.addProperty("App::PropertySpeed",      "FeedRateRotate",    "FeedRate",         "Feed rate while rotating").FeedRateRotate = getParameterFloat("FeedRateRotate", 30)
 
-        obj.addProperty("App::PropertyInteger",    "WireMinPower",      "Wire",             "Minimal wire power").WireMinPower = 500
-        obj.addProperty("App::PropertyInteger",    "WireMaxPower",      "Wire",             "Maximal wire power").WireMaxPower = 1000
+        obj.addProperty("App::PropertyInteger",    "WireMinPower",      "Wire",             "Minimal wire power").WireMinPower = getParameterFloat("WireMinPower", 200)
+        obj.addProperty("App::PropertyInteger",    "WireMaxPower",      "Wire",             "Maximal wire power").WireMaxPower = getParameterFloat("WireMaxPower", 1000)
 
         obj.addProperty("App::PropertyString",     "CutCommand",           "GCODE",         "Command for move while cutting").CutCommand = "G01 {Position} F{FeedRate}"
         obj.addProperty("App::PropertyString",     "MoveCommand",          "GCODE",         "Command for move with cold wire").MoveCommand = "G00 {Position} F{FeedRate}"
@@ -49,15 +49,16 @@ class MachineConfig:
         obj.addProperty("App::PropertyString",     "HomingCommand",        "GCODE",         "Command for homing procedure").HomingCommand = "$H"
         obj.addProperty("App::PropertyString",     "InitPositionCommand",  "GCODE",         "Command for initialize position").InitPositionCommand = "G92 {Position}"
 
-        obj.addProperty("App::PropertyDistance",   "SafeHeight",           "Travel",        "Safe height for travel").SafeHeight = 200
-        obj.addProperty("App::PropertyDistance",   "OriginX",              "Travel",        "Origin along X axis").OriginX = 0
+        obj.addProperty("App::PropertyDistance",   "SafeHeight",           "Travel",        "Safe height for travel").SafeHeight = getParameterFloat("SafeHeight", 200)
+        obj.addProperty("App::PropertyDistance",   "OriginX",              "Travel",        "Origin along X axis").OriginX = getParameterFloat("OriginX", 0)
 
         obj.addProperty("App::PropertyDistance",   "OriginRotationX",      "Travel",        "Origin of rotation along X axis").OriginRotationX = obj.HorizontalTravel / 2
 
-        obj.addProperty("App::PropertyLength",     "BlockWidth",            "Foam Block",   "Foam block size along wire").BlockWidth = 400
-        obj.addProperty("App::PropertyLength",     "BlockLength",           "Foam Block",   "Foam block size along machine X axis").BlockLength = 300
-        obj.addProperty("App::PropertyLength",     "BlockHeight",           "Foam Block",   "Foam block size along machine Y axis").BlockHeight = 50
-        obj.addProperty("App::PropertyPosition",   "BlockPosition",         "Foam Block",   "Foam block position in machine coordinates (x,y,z) where x - coordinate along wire, y - coordinate along machine X axis, z - coordinate along machine Y axis").BlockPosition = App.Vector(-200.0, float(obj.HorizontalTravel / 2.0) - 150.0, 50.0 )
+        obj.addProperty("App::PropertyLength",     "BlockWidth",            "Foam Block",   "Foam block size along wire").BlockWidth = getParameterFloat("BlockWidth", 400)
+        obj.addProperty("App::PropertyLength",     "BlockLength",           "Foam Block",   "Foam block size along machine X axis").BlockLength = getParameterFloat("BlockLength", 300)
+        obj.addProperty("App::PropertyLength",     "BlockHeight",           "Foam Block",   "Foam block size along machine Y axis").BlockHeight = getParameterFloat("BlockHeight", 50)
+        obj.addProperty("App::PropertyPosition",   "BlockPosition",         "Foam Block",   "Foam block position in machine coordinates (x,y,z) where x - coordinate along wire, y - coordinate along machine X axis, z - coordinate along machine Y axis")
+        obj.BlockPosition = App.Vector(float(-obj.BlockWidth / 2.0), float(obj.HorizontalTravel / 2.0) - float(obj.BlockLength / 2), getParameterFloat("BlockPositionHeight", 50) )
 
         obj.setEditorMode("Group",     3)
         obj.Proxy = self
@@ -100,6 +101,30 @@ class MachineConfigVP:
             self.Object = FreeCAD.ActiveDocument.getObject(state["name"])
             return None
         
+def getParameterFloat(name, default):
+    parameters = App.ParamGet("User parameter:BaseApp/Workbench/FoamcutWB/DefaultMachineConfig")
+    val = parameters.GetFloat(name)
+    if val == 0.0 and val != default:
+        val = default
+        parameters.SetFloat(name, val)
+    return val
+
+def getParameterInt(name, default):
+    parameters = App.ParamGet("User parameter:BaseApp/Workbench/FoamcutWB/DefaultMachineConfig")
+    val = parameters.GetInt(name)
+    if val == 0 and val != default:
+        val = default
+        parameters.SetInt(name, val)
+    return val
+
+def getParameterString(name, default):
+    parameters = App.ParamGet("User parameter:BaseApp/Workbench/FoamcutWB/DefaultMachineConfig")
+    val = parameters.GetString(name)
+    if val == '' and val != default:
+        val = default
+        parameters.GetString(name, val)
+    return val
+
 def createConfig(obj):
     MachineConfig(obj)
     MachineConfigVP(obj.ViewObject)
